bad73ca61189d079c323246a169bfc9ea9900f39
Adam Orczyk <adam.orczyk@gmail.com>
2022-11-20 13:18:08 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -22,61 +22,38 @@ let commandsString = ''
 let commands: Commands = []
 let receivingCommand = false;
 let forceStop = false;
-let allStoped: number[] = []
 
 function messageHandler(receivedString: string) {
-    // basic.pause(1000)
-    // bluetooth.uartWriteString(receivedString + '\n')
-    // return
     let data = receivedString.split(';')
 
     if (data[0] == '<'){
         receivingCommand = true
         basic.clearScreen()
-        basic.showString("T")
-        // led.plot(4, 0)
         commandsString = ''
         commands = []
         return
     } else if (data[0] == '>'){
         receivingCommand = false
-        allStoped.push(1)
-        // led.unplot(4, 0)
-        basic.showIcon(IconNames.Yes)
-        // basic.pause(500)
+        // basic.showIcon(IconNames.Happy)
         bluetooth.uartWriteString("1" + '\n')
-        // music.playTone(Note.C, music.beat())
         return
     } else if (receivingCommand) {
         commandsString += data[0]
     } else if (data[0] == '>>') {
         forceStop = false
-        try {
-            if (commandsString) {
-                commands = JSON.parse(commandsString)
-                // console.log(commands[1])
+        if (commandsString) {
+            commands = JSON.parse(commandsString)
 
-                run(commands)
-            }
-        } catch (err) {
-            bluetooth.uartWriteString(err.message)
+            run(commands)
         }
+
     } else if (data[0] == '!') {
         forceStop = true;
-        bluetooth.uartWriteString("2" + '\n')
-    }
-}
-
-function confirmStop() {
-    allStoped.shift()
-    if (allStoped.length == 0){
-        bluetooth.uartWriteString("2" + '\n')
     }
 }
 
 function run(commands: Commands){
     for (let command of commands){
-        // console.log(command)
 
         if (Array.isArray(command)){
             runCommand(command as number[])
@@ -85,7 +62,7 @@ function run(commands: Commands){
         }
 
         if (forceStop){
-            confirmStop()
+            // confirmStop()
             break;
         }
     }
@@ -93,36 +70,36 @@ function run(commands: Commands){
 
 pfTransmitter.connectIrSenderLed(AnalogPin.P0)
 
-const pfChannels: {[key: number]: PfChannel} = {
-    1: PfChannel.Channel1,
-    2: PfChannel.Channel2,
-    3: PfChannel.Channel3,
-    4: PfChannel.Channel4,
-}
+// const pfChannels: {[key: number]: PfChannel} = {
+//     1: PfChannel.Channel1,
+//     2: PfChannel.Channel2,
+//     3: PfChannel.Channel3,
+//     4: PfChannel.Channel4
+// }
 
-const pfOutputs: { [key: number]: PfOutput } = {
-    1: PfOutput.Red,
-    2: PfOutput.Blue,
-}
+// const pfOutputs: { [key: number]: PfOutput } = {
+//     1: PfOutput.Red,
+//     2: PfOutput.Blue,
+// }
 
-const pfSingleOutput: { [key: number]: PfSingleOutput } = {
-    0: PfSingleOutput.Float,
-    1: PfSingleOutput.Forward1,
-    2: PfSingleOutput.Forward2,
-    3: PfSingleOutput.Forward3,
-    4: PfSingleOutput.Forward4,
-    5: PfSingleOutput.Forward5,
-    6: PfSingleOutput.Forward6,
-    7: PfSingleOutput.Forward7,
-    8: PfSingleOutput.Backward1,
-    9: PfSingleOutput.Backward2,
-    10: PfSingleOutput.Backward3,
-    11: PfSingleOutput.Backward4,
-    12: PfSingleOutput.Backward5,
-    13: PfSingleOutput.Backward6,
-    14: PfSingleOutput.Backward7,
-    15: PfSingleOutput.BrakeThenFloat
-}
+// const pfSingleOutput: { [key: number]: PfSingleOutput } = {
+//     0: PfSingleOutput.Float,
+//     1: PfSingleOutput.Forward1,
+//     2: PfSingleOutput.Forward2,
+//     3: PfSingleOutput.Forward3,
+//     4: PfSingleOutput.Forward4,
+//     5: PfSingleOutput.Forward5,
+//     6: PfSingleOutput.Forward6,
+//     7: PfSingleOutput.Forward7,
+//     8: PfSingleOutput.Backward1,
+//     9: PfSingleOutput.Backward2,
+//     10: PfSingleOutput.Backward3,
+//     11: PfSingleOutput.Backward4,
+//     12: PfSingleOutput.Backward5,
+//     13: PfSingleOutput.Backward6,
+//     14: PfSingleOutput.Backward7,
+//     15: PfSingleOutput.BrakeThenFloat
+// }
 
 function compare(n: number, a: number, b: number){
     if (n == 1) {
@@ -136,8 +113,6 @@ function compare(n: number, a: number, b: number){
     return false
 }
 
-// let lightLevel = 0;
-
 function runCommand(command: Commands){
     let commandNr = command[0];
 
@@ -150,32 +125,98 @@ function runCommand(command: Commands){
     } else if (commandNr == 3) {
         led.unplot(command[1] as number, command[2] as number)
     } else if (commandNr == 4) {
-        pfTransmitter.singleOutputMode(pfChannels[command[1] as number], pfOutputs[command[2] as number], pfSingleOutput[command[3] as number])
+        let channel: PfChannel;
+
+        if (command[1] == 1){
+            channel = PfChannel.Channel1
+        } else if (command[1] == 2) {
+            channel = PfChannel.Channel2
+        } else if (command[1] == 3) {
+            channel = PfChannel.Channel3
+        } else if (command[1] == 4) {
+            channel = PfChannel.Channel4
+        }
+
+        let output: PfOutput;
+
+        if (command[2] == 1){
+            output = PfOutput.Red
+        } else {
+            output = PfOutput.Blue
+        }
+
+        let pfCommand: PfSingleOutput;
+
+        if (command[3] == 0) {
+            pfCommand = PfSingleOutput.Float
+        } else if (command[3] == 1) {
+            pfCommand = PfSingleOutput.Forward1
+        } else if (command[3] == 2) {
+            pfCommand = PfSingleOutput.Forward2
+        } else if (command[3] == 3) {
+            pfCommand = PfSingleOutput.Forward3
+        } else if (command[3] == 4) {
+            pfCommand = PfSingleOutput.Forward4
+        } else if (command[3] == 5) {
+            pfCommand = PfSingleOutput.Forward5
+        } else if (command[3] == 6) {
+            pfCommand = PfSingleOutput.Forward6
+        } else if (command[3] == 7) {
+            pfCommand = PfSingleOutput.Forward7
+        } else if (command[3] == 8) {
+            pfCommand = PfSingleOutput.Backward1
+        } else if (command[3] == 9) {
+            pfCommand = PfSingleOutput.Backward2
+        } else if (command[3] == 10) {
+            pfCommand = PfSingleOutput.Backward3
+        } else if (command[3] == 11) {
+            pfCommand = PfSingleOutput.Backward4
+        } else if (command[3] == 12) {
+            pfCommand = PfSingleOutput.Backward5
+        } else if (command[3] == 13) {
+            pfCommand = PfSingleOutput.Backward6
+        } else if (command[3] == 14) {
+            pfCommand = PfSingleOutput.Backward7
+        } else if (command[3] == 15) {
+            pfCommand = PfSingleOutput.BrakeThenFloat
+        }
+        
+        pfTransmitter.singleOutputMode(channel, output, pfCommand)
     } 
-    // else if (commandNr == 5) {
-    //     control.runInBackground(() => {
-    //         while (true) {
-    //             let data = [input.runningTime(), input.compassHeading()];
+    else if (commandNr == 5) {
+        let t = command[1];
 
-    //             bluetooth.uartWriteString(data.join(',') + '\n')
+        let f = () => {
+            let condition = true;
+            while (!forceStop && condition) {
+                let light = input.lightLevel();
+                let sound = input.soundLevel();
 
-    //             basic.pause(command[2] as number || 20)
-    //         }
-    //     })
-    // }
+                if (t != 3) {
+                    condition = compare(command[2] as number, t == 1 ? light : sound, command[3] as number)
+                }
+
+                run(command[4] as Commands)
+            }
+        }
+
+        t == 4 ? control.runInBackground(f) : f()
+    }
     else if (commandNr == 6) {
         // let out = input.lightLevel();
         control.runInBackground(() => {
-            allStoped.push(1)
             let isTrue = false;
             
             while (!forceStop) {
-                let test = compare(command[1] as number, input.lightLevel(), command[2] as number)
+                let light = input.lightLevel();
+                let sound = input.soundLevel();
+
+                let test = compare(command[2] as number, command[1] == 1 ? light : sound, command[3] as number)
 
                 if (test){
                     if (!isTrue){
                         isTrue = true
-                        run(command[3] as Commands)
+                        run(command[4] as Commands)
                     }
                 } else {
                     isTrue = false
@@ -184,11 +225,7 @@ function runCommand(command: Commands){
                 basic.pause(20)
             }
 
-            confirmStop()
+            // confirmStop()
         })
     }
 }
-
-// basic.forever(() =>{
-//     lightLevel = input.lightLevel();
-// })
