739dbe5eec72759a8d36f975cf9532a10656aeb0
Adam Orczyk <adam.orczyk@gmail.com>
2022-11-27 10:16:47 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,11 +3,12 @@ bluetooth.startUartService()
 led.plot(0, 0)
 
 bluetooth.onBluetoothConnected(function () {
-    led.plot(2,0)
+    led.plot(1,0)
 })
 
 bluetooth.onBluetoothDisconnected(function () {
-    led.plot(4, 0)
+    basic.clearScreen()
+    led.plot(0, 0)
 })
 
 function btSend(str: string){
@@ -15,9 +16,7 @@ function btSend(str: string){
 }
 
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
-    let receivedString = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
-
-    messageHandler(receivedString)
+        messageHandler(bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine)))
 })
 
 type Commands = (number | number[])[]
@@ -26,11 +25,15 @@ let commandsString = ''
 let commands: Commands = []
 let receivingCommand = false;
 let forceStop = false;
+let variables: number[] = []
 
 function messageHandler(receivedString: string) {
     let data = receivedString.split(';')
 
-    if (data[0] == '<'){
+    if (data[0] == '0') {
+        forceStop = true;
+        btSend('0\n')
+    } else if (data[0] == '<'){
         receivingCommand = true
         basic.clearScreen()
         commandsString = ''
@@ -38,6 +41,7 @@ function messageHandler(receivedString: string) {
         return
     } else if (data[0] == '>'){
         receivingCommand = false
+        led.plot(2, 0)
         btSend('1\n')
         return
     } else if (receivingCommand) {
@@ -47,25 +51,17 @@ function messageHandler(receivedString: string) {
         if (commandsString) {
             commands = JSON.parse(commandsString)
 
-            run(commands)
+            control.runInBackground(() => run(commands))
         }
-
-    } else if (data[0] == '!') {
-        forceStop = true;
-        // btSend('2\n')
     }
 }
 
 function run(commands: Commands){
     for (let cmd of commands){
+        runCommand(Array.isArray(cmd) ? cmd as number[] : [cmd as number])()
+
         if (forceStop) {
             break;
-        }
-
-        if (Array.isArray(cmd)){
-            runCommand(cmd as number[])
-        } else {
-            runCommand([cmd as number])
         }
     }
 }
@@ -95,6 +91,18 @@ function getData(id: number){
     else if (id == 6) {
         return input.compassHeading()
     }
+    else if (id == 7) {
+        return input.temperature()
+    }
+    else if (id == 8) {
+        return variables[0]
+    }
+    else if (id == 9) {
+        return variables[1]
+    }
+    else if (id == 10) {
+        return variables[2]
+    }
 
     return 0
 }
@@ -103,53 +111,67 @@ function runCommand(cmd: Commands){
     let id = cmd[0];
 
     if (id == 0) {
-        basic.clearScreen()
+        return () => {
+            basic.clearScreen()
+        }
     } 
     else if (id == 1){
-        btSend([input.runningTime(),getData(cmd[1] as number)].join(';') + '\n')
-    } 
-    else if (id == 2) {
-        led.plot(cmd[1] as number, cmd[2] as number)
-    } else if (id == 3) {
-        led.unplot(cmd[1] as number, cmd[2] as number)
-    } else if (id == 4) {
-        let pfCommand: PfSingleOutput;
-
-        if (cmd[3] == 0) {
-            pfCommand = PfSingleOutput.Float
-        } else if (cmd[3] == 1) {
-            pfCommand = PfSingleOutput.Forward1
-        } else if (cmd[3] == 2) {
-            pfCommand = PfSingleOutput.Forward2
-        } else if (cmd[3] == 3) {
-            pfCommand = PfSingleOutput.Forward3
-        } else if (cmd[3] == 4) {
-            pfCommand = PfSingleOutput.Forward4
-        } else if (cmd[3] == 5) {
-            pfCommand = PfSingleOutput.Forward5
-        } else if (cmd[3] == 6) {
-            pfCommand = PfSingleOutput.Forward6
-        } else if (cmd[3] == 7) {
-            pfCommand = PfSingleOutput.Forward7
-        } else if (cmd[3] == 8) {
-            pfCommand = PfSingleOutput.Backward1
-        } else if (cmd[3] == 9) {
-            pfCommand = PfSingleOutput.Backward2
-        } else if (cmd[3] == 10) {
-            pfCommand = PfSingleOutput.Backward3
-        } else if (cmd[3] == 11) {
-            pfCommand = PfSingleOutput.Backward4
-        } else if (cmd[3] == 12) {
-            pfCommand = PfSingleOutput.Backward5
-        } else if (cmd[3] == 13) {
-            pfCommand = PfSingleOutput.Backward6
-        } else if (cmd[3] == 14) {
-            pfCommand = PfSingleOutput.Backward7
-        } else if (cmd[3] == 15) {
-            pfCommand = PfSingleOutput.BrakeThenFloat
+        return () => {
+            let data = [input.runningTime()]
+            for (let i = 1; i < cmd.length; i++){
+                data.push(getData(cmd[i] as number))
+            }
+            btSend(data.join(',') + '\n')
         }
-        
-        pfTransmitter.singleOutputMode(cmd[1] as PfChannel, cmd[2] as PfOutput, pfCommand)
+    }
+    else if (id == 2) {
+        return () => {
+            led.plot(cmd[1] as number, cmd[2] as number)
+        }
+    } else if (id == 3) {
+        return () => {
+            led.unplot(cmd[1] as number, cmd[2] as number)
+        }
+    } else if (id == 4) {
+        return () => {
+            let pfCommand: PfSingleOutput;
+
+            if (cmd[3] == 0) {
+                pfCommand = PfSingleOutput.Float
+            } else if (cmd[3] == 1) {
+                pfCommand = PfSingleOutput.Forward1
+            } else if (cmd[3] == 2) {
+                pfCommand = PfSingleOutput.Forward2
+            } else if (cmd[3] == 3) {
+                pfCommand = PfSingleOutput.Forward3
+            } else if (cmd[3] == 4) {
+                pfCommand = PfSingleOutput.Forward4
+            } else if (cmd[3] == 5) {
+                pfCommand = PfSingleOutput.Forward5
+            } else if (cmd[3] == 6) {
+                pfCommand = PfSingleOutput.Forward6
+            } else if (cmd[3] == 7) {
+                pfCommand = PfSingleOutput.Forward7
+            } else if (cmd[3] == 8) {
+                pfCommand = PfSingleOutput.Backward1
+            } else if (cmd[3] == 9) {
+                pfCommand = PfSingleOutput.Backward2
+            } else if (cmd[3] == 10) {
+                pfCommand = PfSingleOutput.Backward3
+            } else if (cmd[3] == 11) {
+                pfCommand = PfSingleOutput.Backward4
+            } else if (cmd[3] == 12) {
+                pfCommand = PfSingleOutput.Backward5
+            } else if (cmd[3] == 13) {
+                pfCommand = PfSingleOutput.Backward6
+            } else if (cmd[3] == 14) {
+                pfCommand = PfSingleOutput.Backward7
+            } else if (cmd[3] == 15) {
+                pfCommand = PfSingleOutput.BrakeThenFloat
+            }
+            
+            pfTransmitter.singleOutputMode(cmd[1] as PfChannel, cmd[2] as PfOutput, pfCommand)
+        }
     }
     // Repeat Block
     // Use this block to repeat actions.
@@ -167,40 +189,45 @@ function runCommand(cmd: Commands){
     // work properly.
 
     else if (id == 5 || id == 6) {
-        let p1 = cmd[1] as number;
-        let p2 = cmd[2] as number;
-        let p3 = cmd[3] as number;
-        let st = input.runningTime();
-        
-        let condition = true;
+        return () => {
+            let p1 = cmd[1] as number;
+            let p2 = cmd[2] as number;
+            let p3 = cmd[3] as number;
+            let st = input.runningTime();
+            
+            let condition = true;
 
-        while (!forceStop && condition) {
-            if (p1) {
-                condition = compare(
-                    // p1 == 1 ? (input.runningTime() - st) / 1000 : p1 == 2 ? input.lightLevel() : input.soundLevel(),
-                    p1 == 1 ? (input.runningTime() - st) / 1000 : getData(p1 - 1),
-                    p2, 
-                    p3
-                )
+            while (!forceStop && condition) {
+                if (p1) {
+                    condition = compare(
+                        // p1 == 1 ? (input.runningTime() - st) / 1000 : p1 == 2 ? input.lightLevel() : input.soundLevel(),
+                        p1 == 1 ? (input.runningTime() - st) / 1000 : getData(p1 - 1),
+                        p2, 
+                        p3
+                    )
+                }
+
+                if (cmd[4]){
+                    run(cmd[4] as Commands)
+                }
+
+                basic.pause(20)
             }
-
-            if (cmd[4]){
-                run(cmd[4] as Commands)
-            }
-
-            basic.pause(20)
         }
     }
     else if (id == 7) {
-        control.runInBackground(() => {
-            run(cmd[1] as Commands)
-        })
+        return () => {
+            control.runInBackground(() => {
+                run(cmd[1] as Commands)
+            })
+        }
     }
     else if (id == 8) {
-        control.runInBackground(() => {
-            let isTrue = false;
+        // control.runInBackground(() => {
+        let isTrue = false;
+        return () => {
 
-            while (!forceStop) {
+            // while (!forceStop) {
                 if (compare(
                     getData(cmd[1] as number),
                     cmd[2] as number,
@@ -215,9 +242,24 @@ function runCommand(cmd: Commands){
                 }
 
                 basic.pause(20)
-            }
-        })
+            // }
+        // })
+        }
     }
+    else if (id == 9) {
+        return () => {
+            let a = cmd[3] as number;
+            if (cmd[2] == 1) {
+                variables[cmd[1] as number] = a
+            } else if (cmd[2] == 2) {
+                variables[cmd[1] as number] += a
+            } else if (cmd[2] == 3) {
+                variables[cmd[1] as number] -= a
+            }
+        }
+    }
+
+    return () => {}
 }
 
 // input.onButtonPressed(Button.A, function() {
