60257957aad5db924e1cf4db73bb26c3d94d853e
Adam Orczyk <adam.orczyk@gmail.com>
2022-12-24 20:53:31 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -394,9 +394,9 @@ function runCommand(cmd: Commands){
 // }
 
 
-input.onButtonPressed(Button.A, function() {
-    // commands = [0, [2, 0, 0], [5, 1, 1, 10, [[2, 0, 0], [1, 1], [3, 0, 0], [1, 1]]], [2, 1, 1]] as Commands;
-    // commands = [0, [5, 2, 2, 100, [[2, 0, 0], [1, 1], [3, 0, 0], [1, 1]]], [2, 1, 1]] as Commands;
-    commands = [0, [6, [[-1, 2, 20]]], [2, 1, 1]] as Commands;
-    run(commands)
-})
+// input.onButtonPressed(Button.A, function() {
+//     // commands = [0, [2, 0, 0], [5, 1, 1, 10, [[2, 0, 0], [1, 1], [3, 0, 0], [1, 1]]], [2, 1, 1]] as Commands;
+//     // commands = [0, [5, 2, 2, 100, [[2, 0, 0], [1, 1], [3, 0, 0], [1, 1]]], [2, 1, 1]] as Commands;
+//     commands = [0, [6, [[-1, 2, 20]]], [2, 1, 1]] as Commands;
+//     run(commands)
+// })
