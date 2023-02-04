71c76541da715c410047c2b3a57aa4adc1597bef
Adam Orczyk <adam.orczyk@gmail.com>
2023-02-04 18:29:09 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 // --- Test ---
 
 // input.onButtonPressed(Button.A, function() {
-//     commands = [[5,[[0,3,0,0]],[[6,[[-1,2,10,0]]],[102,1,[0]],[6,[[-1,2,10,0]]]]]] as Commands;
+//     commands = [[5, [[0, 3, 0, 0]], [0, [6, [[-1, 2, 10, 0]]], [102, 1, [0]], [6, [[-1, 2, 10, 0]]]]]] as Commands;
 //     run(commands)
 // })
