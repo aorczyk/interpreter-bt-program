50c24143bddaeed18b2fcc941ceacd3ad722ac76
Adam Orczyk <adam.orczyk@gmail.com>
2023-11-20 21:43:02 +0100

    New version.

--- main.ts
+++ main.ts
@@ -35,7 +35,7 @@ function messageHandler(receivedString: string) {
     if (receivedString == '-s') {
         forceStop = true;
     } else if (receivedString == '-v') {
-        btSend('v1.0.5')
+        btSend('v1.0.6')
     } else if (receivedString == '<') {
         commands = []
         receivingCommand = true
